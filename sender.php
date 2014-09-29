0ec981220e7d5e65c42a57ee209e3d3d25a799f3
Marc Cull <marc.cull@gmail.com>
2014-09-29 12:27:00 -0400

    for the QR code

--- sender.php
+++ sender.php
@@ -132,7 +132,7 @@
           $.getJSON( "addSender.php", { rid:  recordingId, sms: sms, email: email} )
             .done(function( json ) {
               console.log(json);
-              var url = "http://54.165.184.141?id=";
+              var url = "http://54.165.184.141?id=" + json;
               $("#qr").html("<img src='https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=" + url + "'>");
             })
 	    .fail(function( jqxhr, textStatus, error ) {
