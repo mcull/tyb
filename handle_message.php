1a584f6425c140ec707d0e70eed4ebe800243098
Marc Cull <marc.cull@gmail.com>
2014-09-29 09:28:19 -0400

    adding first voice handling...

---
+++ handle_message.php
@@ -0,0 +1,18 @@
+<?php
+ 
+require "Services/Twilio.php";
+include "messages.php";
+ 
+$url = $_REQUEST['RecordingUrl'];
+$caller_id = $_REQUEST['Caller'];
+ 
+if (strlen($url)) {
+    //save recording url and callerid as a message 
+    $new_id = 2974 + addMessage($caller_id, $url);
+ 	
+    $response = new Services_Twilio_Twiml();
+    $response->say('Thank you.  Your message id is $new_id.  Good bye');
+    print $response;
+}
+ 
+?>
---
+++ messages.php
@@ -0,0 +1,94 @@
+<?php
+ 
+//DB Constants - Change to your settings
+$db_host='localhost';
+$db_name='tyb';
+$db_user='tyb';
+$db_passwd='xUyUcRa4';
+ 
+
+ 
+function addMessage($caller_id, $recording_url) {
+    global $db_name, $db_host,$db_user,$db_passwd;
+ 
+    mysql_connect($db_host, $db_user, $db_passwd)
+        or die('Could not connect: ' . mysql_error());
+ 
+    mysql_select_db($db_name) or die('Could not select database');
+ 
+    $caller_id = mysql_real_escape_string($caller_id);
+    $recording_url = mysql_real_escape_string($recording_url);
+ 
+    // Performing SQL query
+    $query = sprintf("insert into messages ("
+        . "message_date,message_from,message_audio_url)"
+        . " values (now(),'%s','%s')", $caller_id,
+        $recording_url);
+ 
+    mysql_query($query) or die('Query failed: ' . mysql_error());
+ 
+    $id = mysql_insert_id();
+    mysql_close();
+    return $id;
+}
+ 
+function getMessages($voicemail_exten,$flag=0){
+    global $db_name, $db_host,$db_user,$db_passwd;
+ 
+    mysql_connect($db_host, $db_user, $db_passwd)
+        or die('Could not connect: ' . mysql_error());
+ 
+    mysql_select_db($db_name) or die('Could not select database');
+ 
+    //make sure inputs are db safe
+    $voicemail_exten = mysql_real_escape_string($voicemail_exten);
+    $flag = mysql_real_escape_string($flag);
+ 
+    // Performing SQL query
+    $query = sprintf("select * from messages where message_flag=%d and "
+        . "message_frn_vmb_extension='%s' order by message_date", $flag,
+        $voicemail_exten);
+ 
+    $result = mysql_query($query) or die('Query failed: ' . mysql_error());
+ 
+    $messages = array();
+    while($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+        $messages[]=$line['message_id'];
+    }
+ 
+    mysql_close();
+ 
+    return $messages;
+}
+ 
+function getMessage($msg_id){
+    global $db_name, $db_host,$db_user,$db_passwd;
+ 
+    mysql_connect($db_host, $db_user, $db_passwd)
+        or die('Could not connect: ' . mysql_error());
+ 
+    mysql_select_db($db_name) or die('Could not select database');
+ 
+    //make sure inputs are db safe
+    $msg_id = mysql_real_escape_string($msg_id);
+ 
+    // Performing SQL query
+    $query = sprintf("select * from messages where message_id=%d",$msg_id);
+ 
+ 
+    $result = mysql_query($query) or die('Query failed: ' . mysql_error());
+ 
+    $message = array();
+    if($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+        $message['id']=$line['message_id'];
+        $message['date']=$line['message_date'];
+        $message['from']=$line['message_from'];
+        $message['url']=$line['message_audio_url'];
+    }
+ 
+    mysql_close();
+ 
+    return $message;
+}
+ 
+?>
