c79481c2f6ab66137f4756dd46cd9e4e4fe26749
Marc Cull <marc.cull@gmail.com>
2014-09-29 15:34:37 -0400

    trying to shorten URL

--- sendThanks.php
+++ sendThanks.php
@@ -4,6 +4,35 @@
     include "messages.php";
     include "senders.php";
 
+    function shortenUrl($url) {}
+
+        // Get API key from : http://code.google.com/apis/console/
+        $apiKey = 'MyAPIKey';
+
+        $postData = array('longUrl' => $url, 'key' => 'AIzaSyCqzoVivkKMpHK48Xxa5qPSccMSAUrkyfY');
+        $jsonData = json_encode($postData);
+
+        $curlObj = curl_init();
+
+        curl_setopt($curlObj, CURLOPT_URL, 'https://www.googleapis.com/urlshortener/v1/url');
+        curl_setopt($curlObj, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curlObj, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($curlObj, CURLOPT_HEADER, 0);
+        curl_setopt($curlObj, CURLOPT_HTTPHEADER, array('Content-type:application/json'));
+        curl_setopt($curlObj, CURLOPT_POST, 1);
+        curl_setopt($curlObj, CURLOPT_POSTFIELDS, $jsonData);
+
+        $response = curl_exec($curlObj);
+
+        // Change the response json string to object
+        $json = json_decode($response);
+
+        curl_close($curlObj);
+
+        return $json->id;
+    }
+
+
     $gifter = getSender($_REQUEST['sid']);
 
     $giftMessage = getMessage($gifter['recordingId']);
@@ -22,12 +51,15 @@
 
     $client = new Services_Twilio($AccountSid, $AuthToken);
  
-    
- 
+    $messageText =  "Message from $name via ThankYouBack: \"$message\"";
+    if (strlen($voxMessageId) && strlen($voxUrl)) {
+        $messageText .= " Hear their message: " . shortenUrl($voxUrl);
+    }
+
     $sms = $client->account->messages->sendMessage(
         "646-374-2529", 
         $gifterPhone,
-        "Message from $name via ThankYouBack: \"$message\"",
+       ,
         array($voxUrl)
     );
     echo '{"sucess":"true"}';
