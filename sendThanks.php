bbe865839b912810d708f9e500972cb409da0cb1
Marc Cull <marc.cull@gmail.com>
2014-09-29 14:31:21 -0400

    sending logic

--- sendThanks.php
+++ sendThanks.php
@@ -4,9 +4,16 @@
     include "messages.php";
     include "senders.php";
 
-    $gifter = getSender($_REQUEST['rid']);
+    $gifter = getSender($_REQUEST['sid']);
+
     $giftMessage = getMessage($gifter['recordingId']);
-    $gifterPhone = $giftMessage['caller_id'];
+    $gifterPhone = $giftMessage['from'];
+    $message = $_REQUEST['message'];
+    $name = $_REQUEST['name'];
+    $email = $_REQUEST['email'];
+    $voxMessageId = $_REQUEST['voxId'];
+
+    $voxUrl = getMessage($voxMessageId)['url'];
  
     // Step 2: set our AccountSid and AuthToken from www.twilio.com/user/account
     $AccountSid = "AC6efaec04515e614f6e90b9a8962e70cf";
@@ -15,17 +22,14 @@
 
     $client = new Services_Twilio($AccountSid, $AuthToken);
  
-    //get the sender phone number associated with this package
-    $people = array(
-        "+15026408205" => "Marc Cull"
-    );
-    $number = 
- 
-    $message = $_REQUEST['message'];
+    
  
     $sms = $client->account->messages->sendMessage(
         "646-374-2529", 
         $gifterPhone,
-        $message
+        "Message from $name via ThankYouBack: \"$message\""
     );
+    echo "{'status':'success'}";
+?>
+
     
